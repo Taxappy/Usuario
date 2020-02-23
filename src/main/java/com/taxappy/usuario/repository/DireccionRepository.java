448ad3d396b0a2f788eec9975bcc8e4f1de11dcd
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-02-23 16:52:33 -0500

    microservicio actualizado y funcionando

--- src/main/java/com/taxappy/usuario/repository/DireccionRepository.java
+++ src/main/java/com/taxappy/usuario/repository/DireccionRepository.java
@@ -1,8 +1,10 @@
-package com.taxapy.usuarioservice.repository;
+package com.taxappy.usuario.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.taxapy.usuarioservice.model.Direccion;
+import com.taxappy.usuario.model.Direccion;
+
+
 
 public interface DireccionRepository extends CrudRepository<Direccion, Integer> {
 	Direccion findDireccionByDireccion(String direccion);
